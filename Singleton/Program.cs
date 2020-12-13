6eecbddb496926e6460fcf621a0d7d45cefeb254
David Tolosa <davidtolosa5@gmail.com>
2020-12-12 22:58:58 -0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Singleton singleton1 = new Singleton();
+
+            Singleton singleton2 = Singleton.GetInstance;
+
+            Singleton singleton3 = Singleton.GetInstance;
+
+            Console.Read();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton Instance;
+        private static int TotalInstances;
+
+        private Singleton() {
+            Console.WriteLine("--Private constructor is called.");
+            Console.WriteLine("--Exit from private constructor.");
+        }
+
+        static Singleton() {
+
+            Console.WriteLine("-Static constructor is called.");
+            Instance = new Singleton();
+            TotalInstances++;
+            Console.WriteLine($"-Singleton instance is created. Number of instances: {TotalInstances}");
+            Console.WriteLine("Exit from static constructor");
+        }
+
+        public static Singleton GetInstance => Instance;
+
+    }
+}
